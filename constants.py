0693c78e6d5d5ba87181f69d64c3bab8eba782fa
Moreno La Quatra <moreno.laquatra@gmail.com>
2022-07-10 00:09:56 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,127 @@
+STANDARD_TEMPLATES = {
+    "AGMY": [
+        "A [GENRE] song with a [MOOD] mood from [ARTIST] composed during the [YEAR]",
+        "A [GENRE] track with a [MOOD] mood by [ARTIST] produced during the [YEAR]",
+        "[GENRE] song having [MOOD] mood by [ARTIST] in the [YEAR]",
+        "A [GENRE] [MOOD] masterpiece by [ARTIST] written in the [YEAR]",
+        "A [ARTIST] song of [GENRE] genre with a [MOOD] mood written during the [YEAR]",
+        "[ARTIST] song of [GENRE] genre composed during the [YEAR] having [MOOD] mood",
+        "A song composed in the [YEAR] by [ARTIST] having a [MOOD] mood and [GENRE] genre",
+        "[GENRE] [ARTIST] [YEAR] [MOOD]",
+        "[ARTIST] [GENRE] [MOOD] [YEAR]",
+    ], 
+    "AMY": [
+        "A [MOOD] composition by [ARTIST] written during the [YEAR]",
+        "A song by [ARTIST] written in the [YEAR] and [MOOD] mood",
+        "A track by [ARTIST] composed in the [YEAR] having a [MOOD] mood",
+        "A [MOOD] track written by [ARTIST] in the [YEAR] ",
+        "A [ARTIST] song having a [MOOD] mood written during the [YEAR] ",
+        "A [ARTIST] song with a [MOOD] mood of [YEAR] ",
+        "[ARTIST] [MOOD] [YEAR]",
+        "[MOOD] [YEAR] [ARTIST]",
+    ],
+    "AGM": [
+        "A [GENRE] composition with a [MOOD] mood by [ARTIST]",
+        "A [ARTIST] song of genre [GENRE] having a [MOOD] mood",
+        "A song of [ARTIST] of genre [GENRE] with a [MOOD] mood",
+        "A [ARTIST] track of genre [GENRE] with a [MOOD] mood",
+        "A [GENRE] masterpiece by [ARTIST] having a [MOOD] mood",
+        "[ARTIST] [GENRE] [MOOD]",
+        "[GENRE] [ARTIST] [MOOD]",
+    ],
+    "AGY": [
+        "A [GENRE] song by [ARTIST] composed during the [YEAR]",
+        "A [GENRE] track composed in the [YEAR] by [ARTIST]",
+        "A song of [ARTIST] from [YEAR] having genre [GENRE]",
+        "A [ARTIST] song written during the [YEAR] of genre [GENRE]",
+        "A composition of [ARTIST] written in the [YEAR] of genre [GENRE]",
+        "[ARTIST] [YEAR] [GENRE]",
+        "[GENRE] [ARTIST] [YEAR]",
+    ],
+    "GMY": [
+        "A [GENRE] piece with a [MOOD] mood composed in the [YEAR]",
+        "A [GENRE] track with a [MOOD] mood written during the [YEAR]",
+        "A [GENRE] [MOOD] piece written in [YEAR]",
+        "A song composed during the [YEAR] having a [MOOD] mood and [GENRE] genre",
+        "[GENRE] [MOOD] [YEAR]",
+        "[MOOD] [GENRE] [YEAR]",
+        "[YEAR] [GENRE] [MOOD]",
+    ],
+    "AG": [
+        "A [GENRE] song by [ARTIST]",
+        "A [GENRE] track written by [ARTIST]",
+        "A [GENRE] song composed by [ARTIST]",
+        "A [ARTIST] song of [GENRE] genre",
+        "[GENRE] [ARTIST]",
+        "[ARTIST] [GENRE]",
+    ],
+    "AM": [
+        "A song with [MOOD] mood produced by [ARTIST]",
+        "A [MOOD] track produced by [ARTIST]",
+        "A track by [ARTIST] having mood [MOOD]",
+        "[MOOD] [ARTIST]",
+        "[ARTIST] [MOOD]"
+    ],
+    "AY": [
+        "A composition by [ARTIST] written in the [YEAR]",
+        "A song written by [ARTIST] during the [YEAR]",
+        "A track from [YEAR] written by [ARTIST]",
+        "[YEAR] [ARTIST]",
+        "[ARTIST] [YEAR]",
+    ],
+    "GM": [
+        "A [GENRE] piece with a [MOOD] mood",
+        "A [GENRE] track with a [MOOD] mood"
+        "A [MOOD] [GENRE] song",
+        "A [GENRE] [MOOD] song",
+    ],
+    "GY": [
+        "A [GENRE] music song of [YEAR]",
+        "A track composed during the [YEAR] having [GENRE] genre",
+        "[GENRE] [YEAR]",
+        "[YEAR] [GENRE]",
+    ],
+    "MY": [
+        "[YEAR] composition of mood [MOOD]",
+        "[MOOD] song composed in [YEAR]",
+        "[YEAR] song having [MOOD] mood",
+        "[YEAR] [MOOD]",
+    ],
+    "A": [
+        "A song by [ARTIST]"
+        "A track composed by [ARTIST]"
+        "[ARTIST]"
+    ],
+    "G": [
+        "A [GENRE] song",
+        "A [GENRE] masterpiece",
+        "A track of genre [GENRE]",
+        "[GENRE]"
+    ],
+    "M": [
+        "A song with [MOOD] mood",
+        "A [MOOD] song",
+        "A [MOOD] track",
+        "A [MOOD] track",
+        "[MOOD]"
+    ],
+    "Y": [
+        "A music piece composed during the [YEAR]",
+        "A track from [YEAR]",
+        "A song of [YEAR]",
+        "A track produced in the [YEAR]",
+        "A masterpiece written during the [YEAR]",
+        "[YEAR]"
+    ]
+}
+
+
+'''
+
+Musical Era Tag
+Mood Tags
+Genre Tags ++
+Voice Tags
++ 
+
+'''
